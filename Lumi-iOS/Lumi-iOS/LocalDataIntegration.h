81f9991e537f7cc29b4918d7bc6e9e13319a7275
Martin Kuvandzhiev <martinkuvandzhiev@gmail.com>
2016-06-06 22:44:29 +0300

    Added user data class

---
+++ Lumi-iOS/Lumi-iOS/LocalDataIntegration.h
@@ -0,0 +1,24 @@
+//
+//  LocalDataIntegration.h
+//  Lumi-iOS
+//
+//  Created by Martin Kuvandzhiev on 6/6/16.
+//  Copyright © 2016 Rapid Development Crew. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocalDataIntegration : NSObject
+{
+    NSUserDefaults * userDefaults;
+}
+@property (strong, nonatomic) NSString * username;
+@property (strong, nonatomic) NSString * password;
+@property (strong, nonatomic) NSString * facebookId;
+
+@property (assign, nonatomic) bool rememberPassword;
+@property (assign, nonatomic) bool autoLogin;
+
+@property (strong, nonatomic) NSMutableDictionary * savedUserdata;
+
+@end
---
+++ Lumi-iOS/Lumi-iOS/LocalDataIntegration.m
@@ -0,0 +1,96 @@
+//
+//  LocalDataIntegration.m
+//  Lumi-iOS
+//
+//  Created by Martin Kuvandzhiev on 6/6/16.
+//  Copyright © 2016 Rapid Development Crew. All rights reserved.
+//
+
+#import "LocalDataIntegration.h"
+
+@implementation LocalDataIntegration
++ (instancetype)sharedInstance
+{
+    static LocalDataIntegration *sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[LocalDataIntegration alloc] init];
+    });
+    return sharedInstance;
+}
+
+- (instancetype) init
+{
+    self = [super init];
+    if(self)
+    {
+        userDefaults = [NSUserDefaults standardUserDefaults];
+    }
+    return self;
+}
+
+
+- (NSString *) username
+{
+    return [userDefaults objectForKey:@"username"];
+}
+
+- (void) setUsername:(NSString *)username
+{
+    [userDefaults setObject:username forKey:@"username"];
+}
+
+- (NSString *) password
+{
+    return [userDefaults objectForKey:@"password"];
+}
+
+- (void) setPassword:(NSString *)password
+{
+    [userDefaults setObject:password forKey:@"password"];
+}
+
+
+- (NSString *) facebookId
+{
+    return [userDefaults objectForKey:@"facebookId"];
+}
+
+- (void) setFacebookId:(NSString *)facebookId
+{
+    [userDefaults setObject:facebookId forKey:@"facebookId"];
+}
+
+- (bool) rememberPassword
+{
+    return [userDefaults boolForKey:@"rememberPassword"];
+}
+
+- (void)setRememberPassword:(bool)rememberPassword
+{
+    [userDefaults setBool:rememberPassword forKey:@"rememberPassword"];
+}
+
+- (bool) autoLogin
+{
+    return [userDefaults boolForKey:@"autoLogin"];
+}
+
+- (void) setAutoLogin:(bool)autoLogin
+{
+    [userDefaults setBool:autoLogin forKey:@"autoLogin"];
+}
+
+- (NSMutableDictionary *) savedUserdata
+{
+    return [userDefaults objectForKey:@"savedUserdata"];
+}
+
+- (void) setSavedUserdata:(NSMutableDictionary *)savedUserdata
+{
+    [userDefaults setObject:savedUserdata forKey:@"savedUserdata"];
+}
+
+
+
+@end
